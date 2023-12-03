a3309c357adb232018622139fd496c8b493c393e
Chitopan26 <francisco.catrileo2001@alumnos.ubiobio.cl>
2023-12-03 12:55:50 -0300

    intento de poder empezar

---
+++ build/CMakeFiles/Sequential.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sequential.dir/Secuencial/Secuencial.cpp.obj"
+  "CMakeFiles/Sequential.dir/Secuencial/Secuencial.cpp.obj.d"
+  "Sequential.exe"
+  "Sequential.exe.manifest"
+  "Sequential.pdb"
+  "libSequential.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sequential.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
